2c8addb75ec8b15c2680b4baa5322b1614628c18
cloudflips32 <devadamstandish@gmail.com>
2025-07-06 13:55:23 -0400

    fixed NextJS error within iframe, wave svg viewbox adjusted to span across mobile width

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -24,14 +24,14 @@ export default function Hero() {
 
           {/* Area to display the map */}
           <div className="mt-4 lg:mt-8 mb-8 flex justify-center">
-            <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d265.2674212044669!2d-81.8864846187229!3d26.55230718082615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88db3fed6a955bfb%3A0x71f6c1303d5438f8!2sBuilding%20K%20(Hendry%20Hall)!5e0!3m2!1sen!2sus!4v1751758080377!5m2!1sen!2sus" 
-              width="100%" 
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d265.2674212044669!2d-81.8864846187229!3d26.55230718082615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88db3fed6a955bfb%3A0x71f6c1303d5438f8!2sBuilding%20K%20(Hendry%20Hall)!5e0!3m2!1sen!2sus!4v1751758080377!5m2!1sen!2sus"
+              width="100%"
               height="300"
-              className="rounded-xl shadow-lg transition-transform duration-700" 
+              className="rounded-xl shadow-lg transition-transform duration-700"
               style={{ border: 0 }}
-              allowfullscreen="" 
-              loading="lazy" 
+              allowFullScreen=""
+              loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
               title="Florida SouthWestern State College Map">
             </iframe>
