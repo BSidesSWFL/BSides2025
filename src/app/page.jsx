cd512f80c43b14d571371ad9ff94b7a3542082a1
cloudflips32 <devadamstandish@gmail.com>
2025-07-06 14:31:03 -0400

    call for speakers modal disabled per #84

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,7 +24,7 @@ export default function Home() {
         <Hero />
         <InfoCards />
       </main>
-      <CallForSpeakersModal isOpen={isModalOpen} onClose={handleCloseModal} />
+      {/* <CallForSpeakersModal isOpen={isModalOpen} onClose={handleCloseModal} /> */}
     </>
   );
 }
