40865d7954999b8db9c3a0ebfe07f22c80fc0605
Crystal Wernicke <71859372+CrystalWernicke@users.noreply.github.com>
2025-10-14 12:20:23 -0400

    Refactor village page structure and content
    Updated the village page layout and removed old content.

--- src/app/villages/page.jsx
+++ src/app/villages/page.jsx
@@ -12,24 +12,8 @@ export default function Villages() {
         subtitle="Villages are all-day interactive spaces where attendees can get hands-on, ask questions, and explore niche areas of security. Whether it's lockpicking, social engineering, or career building, villages turn curiosity into connection through real-world experience."
       />
 
-      <div className="max-w-5xl mx-auto px-6 py-16 text-slate-800 text-center">
-        <h2 className="text-2xl md:text-3xl font-bold mb-4">Call for Villages Now Open</h2>
-        <p className="text-lg text-slate-700 mb-4">
-          Have an idea for a hands-on, interactive space that brings people together around a shared area of interest in cybersecurity? BSides SWFL is accepting village proposals for Saturday, November 15th.
-        </p>
-        <p className="text-lg text-slate-700 mb-4">
-          Villages are open all day and give attendees the chance to explore, experiment, and engage — whether it’s lockpicking, social engineering, hardware hacking, career coaching, or something brand new.
-        </p>
-        <p className="text-lg text-slate-700 mb-4">
-          We’re looking for creative and inclusive experiences. Tell us your vision, how attendees will interact, and what you’ll need to make it happen.
-        </p>
-      </div>
-
-      <div className="text-center -mt-10 pb-44 md:pb-45">
-        <a href='https://sessionize.com/bsidesswfl2025/'>
-          <button className="bg-teal-600 text-white font-bold py-3 px-8 rounded-lg shadow-md hover:bg-teal-700 transition-colors duration-200 w-[90%] md:w-auto"> Reach Out Today! <img className="inline-block w-12 h-12 ml-4" src="bsideslogo.png" />
-          </button>
-        </a>
+      <div className="max-w-5xl mx-auto wrapper-pages p-4 md:mb-12 pt-4 -mt-12">
+        <div class="hs-form-frame mt-6 mb-14" data-region="na2" data-form-id="fb4869ea-c35c-4d70-9ddb-7508f9d6cf1f" data-portal-id="242985282">
       </div>
     </main>
   )
