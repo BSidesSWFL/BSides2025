d6f1dae27356afc886f46e1b7bb42373de30e493
Crystal Wernicke <71859372+CrystalWernicke@users.noreply.github.com>
2025-11-10 09:47:18 -0500

    Update iframe height from 800 to 1000
    Increased the height of iframes for better display.

--- src/app/workshops/page.jsx
+++ src/app/workshops/page.jsx
@@ -26,7 +26,7 @@ export default function Workshops() {
               <iframe
                 src="https://luma.com/embed/event/evt-4q1dQwn9hZwCPA3/simple"
                 width="100%"
-                height="800"
+                height="1000"
                 frameBorder="0"
                 style={{
                   border: "1px solid #bfcbda88", 
@@ -47,7 +47,7 @@ export default function Workshops() {
               <iframe
                 src="https://luma.com/embed/event/evt-9x9hYPSj6KvYxLU/simple"
                 width="100%"
-                height="800"
+                height="1000"
                 frameBorder="0"
                 style={{
                   border: "1px solid #bfcbda88", 
